129c1113500958f99cd9f2f84d0afe0e9b00501c
renehorschinger <r.hoerschinger@gmx.at>
2021-01-05 11:42:04 +0100

    first prototype for learning

--- src/train_model.py
+++ src/train_model.py
@@ -5,8 +5,9 @@ import os
 import xml.etree.ElementTree as ET
 import sklearn.model_selection
 import numpy as np
+import matplotlib.image as mpimg
 
-IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 268, 500, 3
+IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 128, 128, 3
 INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
 
 def build_model(input_shape):
@@ -43,7 +44,7 @@ def readXML(xmlname, path=None):
     bndbox = []
     for element in bndbox_elements:
         bndbox.append(int(element.text))
-    return (cv2.imread(path + "/images/" + filename), bndbox)
+    return filename, bndbox
 
 def load_data():
     print("loading data...")
@@ -55,16 +56,36 @@ def load_data():
         image, box = readXML(file)
         input.append(image)
         output.append(box)
-
+    input = np.array(input)
+    output = np.array(output)
     input_train, input_test, output_train, output_test = sklearn.model_selection.train_test_split(input, output,
                                                                                                   test_size=0.2,
                                                                                                   random_state=0)
-
     return input_train, input_test, output_train, output_test
+
+def load_image(data_dir, image_file):
+    img = mpimg.imread(os.path.join(data_dir, os.path.basename(image_file)))
+    if len(img.shape) > 2 and img.shape[2] == 4:
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    img = cv2.resize(img, (IMAGE_HEIGHT, IMAGE_WIDTH))
+    return img
 
 def batch_generator(data_dir, image_paths, box, batch_size):
     images = np.empty([batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
-    steers = np.empty(batch_size)
+    boxes = np.empty([batch_size, 4])
+    while True:
+        i = 0
+        for index in np.random.permutation(image_paths.shape[0]):
+            img = image_paths[index]
+            image = load_image(data_dir, img)
+            bounding_box = box[index]
+
+            images[i] = image
+            boxes[i] = bounding_box
+            i += 1
+            if i == batch_size:
+                break
+        yield images, boxes
 
 def train_model(model, input_train, input_test, output_train, output_test):
     checkpoint = tf.keras.callbacks.ModelCheckpoint('model-{epoch:03d}.h5',
@@ -75,10 +96,16 @@ def train_model(model, input_train, input_test, output_train, output_test):
 
     model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(lr=1.0e-4))
 
-    #model.fit(input_train, output_train, epochs=8, callbacks=[checkpoint])
-    print(model.evaluate(input_test, output_test))
+    model.fit(batch_generator('../pics/train_dataset/images', input_train, output_train, 40),
+              steps_per_epoch=2000,
+              epochs=5,
+              max_queue_size=10,
+              validation_data=batch_generator('../pics/train_dataset/images', input_test, output_test, 40),
+              validation_steps=len(input_test),
+              callbacks=[checkpoint],
+              verbose=1)
 
 if __name__ == "__main__":
     data = load_data()
     model = build_model(INPUT_SHAPE)
-    #train_model(model, *data)
+    train_model(model, *data)
