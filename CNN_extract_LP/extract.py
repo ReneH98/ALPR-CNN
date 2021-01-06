e8b65a5f9be28bdf1411887a3c16dafb4becccdb
renehorschinger <r.hoerschinger@gmx.at>
2021-01-06 11:01:08 +0100

    renamed file

--- CNN_extract_LP/extract.py
+++ CNN_extract_LP/extract.py
@@ -15,12 +15,17 @@ from tensorflow.keras.models import load_model
 
 def extract_LP(image_name,base_model_name):
     original_img = cv2.imread(image_name)
-    og_shape = original_img.shape
+    h, w, _ = original_img.shape
 
-    compute_img = cv2.resize(original_img, (800, 480))
-    h, w, _ = compute_img.shape
+    print("Original: ", w, " ", h)
 
-    image = compute_img[int(0.1*h) : int(0.9*h), int(0.1*w) : int(0.9*w)]
+    new_height = 480
+    compute_img = cv2.resize(original_img, (int(w/h*new_height), new_height))
+
+    nh, nw, _ = compute_img.shape
+    print("Cropped: ", nw, " ", nh)
+
+    image = compute_img[int(0.1*nh) : int(0.9*nh), int(0.1*nw) : int(0.9*nw)]
 
     model = load_model(base_model_name)
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
@@ -59,11 +64,18 @@ def extract_LP(image_name,base_model_name):
         clean_y2 = clean_box[3]
         total_boxes+=1
         cv2.rectangle(copy,(clean_x1,clean_y1),(clean_x2,clean_y2),(0,255,0),3)
-        
-        #print(clean_x1, " ", clean_x2, " ", clean_y1, " ", clean_y2)
+    
+        cropped2 = image[clean_y1 : clean_y2, clean_x1: clean_x2]
 
-        cropped = image[clean_y1:clean_y2, clean_x1: clean_x2]
-        cv2.imshow("possible LP", cropped)
+        y1 = int((clean_y1 + 0.1*nh) / nh * h)
+        y2 = int((clean_y2 + 0.1*nh) / nh * h)
+        x1 = int((clean_x1 + 0.1*nw) / nw * w)
+        x2 = int((clean_x2 + 0.1*nw) / nw * w)
+        cropped = original_img[y1 : y2, x1: x2]
+    
+        cv2.imshow("cropped", cropped)
+        cv2.waitKey(0)
+        cv2.imshow("cropped2", cropped2)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
