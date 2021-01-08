58e0dab1bf42adb76d4b81eae1aef7151c7feff5
renehorschinger <r.hoerschinger@gmx.at>
2021-01-08 09:40:40 +0100

    fix deprecated issue

--- CNN_extract_LP/extract.py
+++ CNN_extract_LP/extract.py
@@ -15,6 +15,7 @@ def extract_LP(image_path, image_name, model, model_name=""):
     nh, nw, _ = compute_img.shape
     #print("Cropped: ", nw, " ", nh)
 
+    image = compute_img
     image = compute_img[int(0.1*nh) : int(0.9*nh), int(0.1*nw) : int(0.9*nw)]
 
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
@@ -38,14 +39,12 @@ def extract_LP(image_path, image_name, model, model_name=""):
         roi = image.copy()[y1:y2,x1:x2]
         roi = cv2.resize(roi,(128,128))
         roi_use = roi.reshape((1,128,128,3))
-        class_pred = model.predict_classes(roi_use)[0][0]
 
-        if class_pred == 1:
-            prob = model.predict(roi_use)[0][0]
-            if prob > 0.98:
-                positive_boxes.append([x1,y1,x2,y2])
-                probs.append(prob)
-                #cv2.rectangle(copy2,(x1,y1),(x2,y2),(255,0,0),5)
+        prob = float(model.predict(roi_use)[0])
+        if prob > 0.98:
+            positive_boxes.append([x1,y1,x2,y2])
+            probs.append(prob)
+            #cv2.rectangle(copy2,(x1,y1),(x2,y2),(255,0,0),5)
 
     cleaned_boxes = non_max_suppression_fast(np.array(positive_boxes),0.1,probs)
     total_boxes = 0
@@ -57,17 +56,17 @@ def extract_LP(image_path, image_name, model, model_name=""):
         total_boxes+=1
         cv2.rectangle(copy,(clean_x1,clean_y1),(clean_x2,clean_y2),(0,255,0),3)
 
+        """
         y1 = int((clean_y1 + 0.1*nh) / nh * h)
         y2 = int((clean_y2 + 0.1*nh) / nh * h)
         x1 = int((clean_x1 + 0.1*nw) / nw * w)
         x2 = int((clean_x2 + 0.1*nw) / nw * w)
         cropped = original_img[y1 : y2, x1: x2]
 
-        # cv2.imshow("cropped", cropped)
-        # cv2.waitKey(0)
-        # cv2.imshow("cropped2", cropped2)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        cv2.imshow("cropped", cropped)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        """
 
     #plt.imshow(copy2)
     #plt.imshow(copy)
@@ -79,16 +78,16 @@ def extract_LP(image_path, image_name, model, model_name=""):
 #model_eu = load_model('models/model_eu_only.h5')
 model_eu_15 = load_model('models/model_eu_only_15epochs.h5')
 
-#pic_dir = "../pics/Pictures_FH2/"
-#pic = "05.png"
+pic_dir = "../pics/images/"
+pic = "134.png"
 
 #extract_LP(pic_dir + "/", pic, model_1, "_1")
 #extract_LP(pic_dir + "/", pic, model_2, "_2")
 #extract_LP(pic_dir + "/", pic, model_eu, "_eu8")
-#extract_LP(pic_dir + "/", pic, model_eu_15, "_eu15")
+extract_LP(pic_dir + "/", pic, model_eu_15, "_test")
 
-
-pic_dir = '../pics/test'
+"""
+pic_dir = '../pics/Pictures_FH2'
 import os
 pics = os.listdir(pic_dir)
 pics.sort()
@@ -98,5 +97,5 @@ for pic in pics:
         #extract_LP(pic_dir + "/", pic, model_2, "_2")
         #extract_LP(pic_dir + "/", pic, model_eu, "_eu8")
         #extract_LP(pic_dir + "/", pic, model_eu_15, "_eu15_300")
-        extract_LP(pic_dir + "/", pic, model_eu_15, "_test")
-        
+        extract_LP(pic_dir + "/", pic, model_eu_15, "_test")     
+"""
